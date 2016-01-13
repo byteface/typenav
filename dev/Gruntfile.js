cafd42d88d71bf6575377ea596b5aadfc5dee1dd
michael <michael@rotorstudios.com>
2016-01-13 15:57:42 +1100

    shuffle shit around bit

---
+++ dev/Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    uglify: {
+      options: {
+        sourceMap: true
+      },
+      my_target: {
+        files: {
+         '../dist/typenav.min.js': [ 'typenav.js' ]
+        }
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['uglify'] );
+
+};
